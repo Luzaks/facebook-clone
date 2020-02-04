ce8449c02ab1aabe58361ab73ccf911c9ce8522a
Luzaks <kanemekanik@gmail.com>
2020-02-04 11:57:30 -0600

    feat: Add columns for name and last name to the devise table, style: change the forms in the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    private
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname])
+    end
 end
