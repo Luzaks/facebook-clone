2709e44b8c02fdaa3b262ecf9fbe06746b47478e
Luzaks <kanemekanik@gmail.com>
2020-02-05 13:00:38 -0600

    feat: add integration_spec.rb file with tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,11 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  Devise::Test::IntegrationHelpers
+end
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
