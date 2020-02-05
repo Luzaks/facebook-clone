b5279f584f36298466bc5ab7aff1ea6943e750c2
meli castorena <mcastorena0316@gmail.com>
2020-02-05 14:49:21 -0600

    feat: unit tester style: user page show styling

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create!(name: 'Foo', lastname: 'Bar', email: 'goobar@example.org', password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  context 'with valid details' do
+    it 'should create a user' do
+      expect(@user).to be_valid
+    end
+
+    it 'should have a name' do
+      expect(@user.name).to be_present
+    end
+
+    it 'should have an email' do
+      expect(@user.email).to be_present
+    end
+
+  end
+
+  context 'with invalid details' do
+    it 'should not be valid' do
+      @user.name = "    "
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid' do
+      @user.lastname = "    "
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have a unique email adress' do
+      duplicate_user = @user.dup
+      @user.save
+      expect(duplicate_user).to_not be_valid
+    end
+  end
 end
