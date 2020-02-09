7f3bb46fd36a4fe6315c032430489eb50d7aa788
Luzaks <kanemekanik@gmail.com>
2020-02-08 20:41:23 -0600

    style: add style to posts and comments views, fix: the comments render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name lastname])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name lastname])
   end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   def show
- 
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page])
     @post = @user.posts.build
     @comments = Comment.all
-  
   end
 
   def index
