42e3235f2e5bf417307284aa5e9529f0457834b0
Luzaks <kanemekanik@gmail.com>
2020-02-11 10:31:37 -0600

    feat: add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_221954) do
+ActiveRecord::Schema.define(version: 2020_02_11_162639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 2020_02_10_221954) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["likeable_id"], name: "index_likes_on_likeable_id"
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
+    t.index ["likeable_type"], name: "index_likes_on_likeable_type"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
