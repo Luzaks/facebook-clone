68360813e539707c344e4e45ccf4d153bedf6dff
meli castorena <mcastorena0316@gmail.com>
2020-02-12 13:00:36 -0600

    add counter to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
 
-    def list_pending_friends(user)
-        
-        friendship_2 = @inverse_friendships.map{|friendship| friendship.user if !friendship.friendship_status}.compact
-        friendship_2
-    end
+  
 end
