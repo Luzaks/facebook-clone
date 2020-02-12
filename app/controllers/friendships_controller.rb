603cf28b7de9e8c3225ad371cc5cd5d70ae9ec4a
meli castorena <mcastorena0316@gmail.com>
2020-02-11 22:35:11 -0600

    feat: buttons logic for friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,8 +5,8 @@ class FriendshipsController < ApplicationController
 
   def create
     user = User.find(params[:id])
-    friendship = Friendship.create(user_id: current_user.id, friend_id: user.id, friendship_status: false)
-    if friendship.save
+    @friendship = Friendship.create(user_id: current_user.id, friend_id: user.id, friendship_status: false)
+    if @friendship.save
       flash[:success] = 'Friendship request successfull'
       redirect_to users_path
     else
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,26 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: current_user.name, class: 'gravatar')
   end
+
+
+  # Users who have yet to confirme friend 
+  def pending_for_accept_my_request(user)
+    friendship1= @friendships.map{|friendship| friendship.friend if !friendship.friendship_status}.compact
+    friendship1.include?(user)
+  end
+
+  def pending_for_me_to_accept(user)
+   friendship_2= @inverse_friendships.map{|friendship| friendship.user if !friendship.friendship_status}.compact
+   friendship_2.include?(user)
+  end
+
+  def we_are_friends(user)
+    friends_list = @friendships.map{|friendship| friendship.friend if friendship.friendship_status}
+    friends_list += @inverse_friendships.map{|friendship| friendship.user if friendship.friendship_status}
+    friends_list.compact
+    friends_list.include?(user)
+
+  end
+
+  
 end
