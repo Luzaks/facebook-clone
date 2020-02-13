c0ec928b746d47f4dbd4ecffa5e19559832efdee
Luzaks <kanemekanik@gmail.com>
2020-02-13 11:02:20 -0600

    fix: substract the logic from the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     return true if current_user.friendships.where(user_id: current_user.id, friend_id: post_user, friendship_status: true).exists?
     return true if current_user.inverse_friendships.where(user_id: post_user, friend_id: current_user, friendship_status: true).exists?
   end
+
+  def isit_me?(post_user)
+    post_user == current_user
+  end
 end
